c0011553c4396562fe2674f82434c4999355bb68
Franck D'agostini <franck.dagostini@gmail.com>
2012-03-03 17:45:34 +0100

    removing vendor/ruby from repo

--- vendor/ruby/1.9.1/gems/capybara-webkit-0.9.0/lib/capybara/driver/webkit/version.rb
+++
@@ -1,7 +0,0 @@
-module Capybara
-  module Driver
-    class Webkit
-      VERSION = '0.9.0'.freeze
-    end
-  end
-end
--- vendor/ruby/1.9.1/gems/guard-spork-0.5.2/lib/guard/spork/version.rb
+++
@@ -1,5 +0,0 @@
-module Guard
-  module SporkVersion
-    VERSION = "0.5.2"
-  end
-end
